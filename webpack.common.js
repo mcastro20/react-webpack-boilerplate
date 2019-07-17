78a218654dc439a85ecb6dcda3587fc3c5be4950
michael castro <m.castro@multisoft.com>
2019-07-17 08:32:23 +0800

    Separate webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
-  },
   module: {
     rules: [
       {
