342000d8757f6f2ed416f6bd496e1d1d21a5229e
mcastro20 <michaelanthony.castro26@gmail.com>
2019-07-23 09:09:08 +0800

    Test bootstrap native code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>React Webpack Boiler Plate!</h1>
+        <button type="button" className="btn btn-primary">
+          Test
+        </button>
       </div>
-    )
+    );
   }
 }
 
